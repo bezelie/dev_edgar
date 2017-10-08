1484a12287dbf0cb22e7c346eee849f60a0919ec
bezelie <member@bezelie.com>
2017-10-08 20:11:59 +0900

    m

--- sample_talk1.py
+++ sample_talk1.py
@@ -1,15 +1,25 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Bezelie Sample Code : speak by openJTalk
 
-from time import sleep
 import subprocess
+from time import sleep             # ウェイト処理
+import bezelie                     # べゼリー専用モジュール
+
+openJTalkFile = "/home/pi/bezelie/dev_edgar/exec_openJTalk.sh"  #
+
+# Setting
+bez = bezelie.Control()               # べゼリー操作インスタンスの生成
+bez.setTrim(head=0, back=-5, stage=0) # センター位置の微調整
+bez.moveCenters()                     # ０番から７番までのサーボをセン$
 
 # Main Loop
-try:
-  while (True):
-    subprocess.call('/home/pi/aquestalkpi/AquesTalkPi -s 120 "アクエストーク" | aplay', shell=True)
-    sleep(0.5)
-    subprocess.call('sh /home/pi/bezelie/testpi/openJTalk.sh '+'ジェイトーク', shell=True)
-    sleep(0.5)
-except KeyboardInterrupt:
-  print ' Interrupted by Keyboard'
+def main():
+  try:
+    while (True):
+      subprocess.call("sh "+openJTalkFile+" "+"こんにちわ", shell=True)
+      sleep(0.5)
+  except KeyboardInterrupt:
+    print ' Interrupted by Keyboard'
+
+if __name__ == "__main__":
+    main()
--- test_openJTalk1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from time import sleep             # ウェイト処理
-import subprocess                  #
-
-openJTalkFile = "/home/pi/bezelie/dev_edgar/exec_openJTalk.sh"  #
-
-subprocess.call("sh "+openJTalkFile+" "+"起動します", shell=True)
