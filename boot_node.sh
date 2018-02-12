0f38adb4350d4adbaa8e18e409d3f4a7e18dd0f1
bezelie <member@bezelie.com>
2018-02-12 23:23:15 +0900

    m

--- boot_node.sh
+++ boot_node.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # node server
-/usr/local/nodejs/bin/node /home/pi/bezelie/dev_edgar/server_chat.js &
+/usr/local/nodejs/bin/node /home/pi/bezelie/server_chat.js &
 # echo "starting startup applications"
-# DHCPサービスの起動
-# sudo service isc-dhcp-server start
 exit 0
