d75605d90754c70ad9ff50b2cc87a7fd89c98075
bezelie <member@bezelie.com>
2018-02-12 20:47:17 +0900

    m

--- boot_python.sh
+++ boot_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # pythonスクリプト起動スクリプト
-sleep 12 # 自動起動に失敗する場合、この待ち時間を長くしてみてください
+sleep 13 # 自動起動に失敗する場合、この待ち時間を長くしてみてください
 cd /home/pi/bezelie/dev_edgar
 /usr/bin/python /home/pi/bezelie/dev_edgar/demo_chat1.py
 exit 0
